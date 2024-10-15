1ee176db19deb116d5937c322d91848bc1d2d242
Edmundo Canedo Cervantes <edcance.dev@gmail.com>
2024-10-15 09:42:44 -0600

    Update datos.php

--- datos.php
+++ datos.php
@@ -8,6 +8,7 @@ $result = mysqli_query($connection, $query);
 
 $query = "SELECT datatest FROM mydata";
 $result = mysqli_query($connection, $query);
-while($row=mysqli_fetch_assoc($result)) {?>
-    <p><?php echo $row["datatest"]; ?></p>
-<?php}
+
+while ($row = mysqli_fetch_assoc($result)) {
+    echo "<p>".$row["datatest"]."</p>";
+}
