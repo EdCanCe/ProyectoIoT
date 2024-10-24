e986835327c4c7c80fdb9fc0bfbf00025e50fa99
EdCanCe <edcance.dev@gmail.com>
2024-10-23 23:50:15 -0600

    get data reload on php test

--- dataReloadCall.js
+++ dataReloadCall.js
@@ -6,9 +6,25 @@ let a=5;
 let temperature=document.getElementById("temperatureHolder");
 
 async function dataReload(){
-    temperature.textContent=a;
-    a+=1;
-    await sleep(1000);
+
+    try {
+        // Realizamos la solicitud al archivo PHP
+        const response = await fetch('getLatestRow.php');
+        const data = await response.json(); // Convertimos la respuesta a JSON
+
+        // Comprobamos si hay un error en los datos
+        if (data.error) {
+            console.error(data.error);
+        } else {
+            console.log(data); // Aquí están los datos de la última fila
+            // Puedes actualizar tu página o DOM con los datos recibidos
+            document.getElementById('temperatureHolder').textContent = data.temperatura+" "+a; // Ejemplo
+        }
+    } catch (error) {
+        console.error('Error al obtener los datos:', error);
+    }
+
+    await sleep(5000); //5 segundos
     dataReload();
 }
 
