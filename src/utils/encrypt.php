6afc0c400f61ced2bf6c382fe14adc8ca0827f7c
🌿 Edmundo Canedo Cervantes <95944066+EdCanCe@users.noreply.github.com>
2024-11-29 04:00:38 -0600

    Create encrypt.php

---
+++ src/utils/encrypt.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Encripta una cadena de texto.
+ * 
+ * @param string $raw La cadena de texto a encriptar.
+ * @param int $size El tamaño final que tendrá la cadena.
+ * 
+ * @return string La cadena encriptada.
+ */
+function encrypt($raw, $size){
+   
+    return $raw;
+}
